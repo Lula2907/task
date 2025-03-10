824dc5bc1cfefc4b2bad77f0645572fb74d41c86
Aman Reja <amnmolla742@gmail.com>
2025-03-10 09:28:45 +0530

    fixed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,50 +1,90 @@
 import React from "react";
-import adminpng from "../assets/images/admin.png";
-import "../App.css";
 import { Link } from "react-router-dom";
+import Studentpng from "../assets/images/student.jpg";
 
 const Home = () => {
   return (
     <>
-      <section className="liniar1 h-[700px]">
-        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-          <div className="mr-auto place-self-center relative sm:bottom-[90px] buttom-[10px] lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
-              Unlock Your Tech Career with Our Expert-Led Courses!
+      <section className="relative overflow-hidden bg-gradient-to-b from-blue-50 via-transparent to-transparent pb-12 pt-20 sm:pb-16 sm:pt-32 lg:pb-24 xl:pb-32 xl:pt-40">
+        <div className="relative z-10">
+          <div className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]">
+            <svg
+              className="h-[60rem] w-[100rem] flex-none stroke-blue-600 opacity-20"
+              aria-hidden="true"
+            >
+              <defs>
+                <pattern
+                  id="e9033f3e-f665-41a6-84ef-756f6778e6fe"
+                  width={200}
+                  height={200}
+                  x="50%"
+                  y="50%"
+                  patternUnits="userSpaceOnUse"
+                  patternTransform="translate(-100 0)"
+                >
+                  <path d="M.5 200V.5H200" fill="none" />
+                </pattern>
+              </defs>
+              <svg x="50%" y="50%" className="overflow-visible fill-blue-50">
+                <path
+                  d="M-300 0h201v201h-201Z M300 200h201v201h-201Z"
+                  strokeWidth={0}
+                />
+              </svg>
+              <rect
+                width="100%"
+                height="100%"
+                strokeWidth={0}
+                fill="url(#e9033f3e-f665-41a6-84ef-756f6778e6fe)"
+              ></rect>
+            </svg>
+          </div>
+        </div>
+        <div className="relative z-20 mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Unlock Your Tech Career with Our
+              <span className="text-blue-600">Expert-Led Courses!</span>
             </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            <h2 className="mt-6 text-lg leading-8 text-gray-600">
               Master Frontend, Backend, and Full-Stack development with our
               industry-focused courses. Learn from experts, build real-world
               projects, and elevate your coding skills. Enroll now and take the
-              next step toward your dream career! 🚀
-            </p>
-            <Link
-              to="/adminlogin"
-              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 bg-lime-400"
-            >
-              Admin
-              <svg
-                className="w-5 h-5 ml-2 -mr-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+              next step toward your dream career!
+            </h2>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                to="/adminlogin"
+                className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 bg-lime-400"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Link>
-            <Link
-              to="/usersignup"
-              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 bg-pink-600"
-            >
-              User
-            </Link>
+                Admin
+                <svg
+                  className="w-5 h-5 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Link>{" "}
+              <Link
+                to="/usersignup"
+                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 bg-violet-600 w-[200px]"
+              >
+                User
+              </Link>
+            </div>
           </div>
-          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex  ">
-            <img className="rounded-full" src={adminpng} alt="mockup" />
+          <div className="relative mx-auto mt-10 max-w-lg">
+            <img
+              className="w-full rounded-2xl border border-gray-100 shadow"
+              src={Studentpng}
+              alt
+            />
           </div>
         </div>
       </section>
