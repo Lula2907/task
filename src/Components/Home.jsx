4e9714766dcccabb0118f555637dc84c18d64782
Aman Reja <amnmolla742@gmail.com>
2025-03-10 09:30:17 +0530

    fixed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -73,7 +73,7 @@ const Home = () => {
               </Link>{" "}
               <Link
                 to="/usersignup"
-                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 bg-violet-600 w-[200px]"
+                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 bg-violet-600 w-[200px]"
               >
                 User
               </Link>
